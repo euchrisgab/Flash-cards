93cabfa4c8e9f312803112db43c33a0b982995ee
euchrisgab <christofergabriel06@gmail.com>
2025-05-28 17:31:58 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,59 @@
+criaCartao(
+    'Matemática',
+    'Qual é o valor de π (pi) arredondado para duas casas decimais?',
+    '3,14.'
+)
+
+criaCartao(
+    'Literatura',
+    'Quem escreveu “Dom Casmurro”?',
+    'Machado de Assis.'
+)
+
+criaCartao(
+    'Física',
+    'Qual é a unidade de medida da força?',
+    'Newton.'
+)
+
+criaCartao(
+    'Biologia',
+    'Qual órgão é responsável por bombear o sangue no corpo humano?',
+    'O coração.'
+)
+
+criaCartao(
+    'História',
+    'Em que ano ocorreu a Proclamação da República no Brasil?',
+    '1889.'
+)
+
+criaCartao(
+    'Tecnologia',
+    'O que significa a sigla “HTML”?',
+    'HyperText Markup Language.'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é o país com maior população do mundo?',
+    'China.'
+)
+
+criaCartao(
+    'Química',
+    'Qual é o número atômico do hidrogênio?',
+    '1.'
+)
+
+criaCartao(
+    'Arte',
+    'Qual movimento artístico é conhecido por obras com distorções e cores fortes, como as de Van Gogh?',
+    'Expressionismo.'
+)
+
+criaCartao(
+    'Astronomia',
+    'Qual é o satélite natural da Terra?',
+    'A Lua.'
+)
